01d52c72c1384905317f93eb1cdcd6930cddd9f4
Sergey Tetenko <80429666+Nalfeun@users.noreply.github.com>
2021-11-17 23:53:06 +0200

    nine homework

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const emailVal = event.currentTarget.elements.email.value;
+  const passwordVal = event.currentTarget.elements.password.value;
+
+  if (emailVal === "" || passwordVal === "") {
+    alert("Fill in all the fields!");
+  }
+
+  console.log({ emailVal, passwordVal });
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeBgColorBtn = document.querySelector(".change-color");
+const changeBgColorTxt = document.querySelector(".color");
+
+changeBgColorBtn.addEventListener("click", onChangeBgColor);
+
+function onChangeBgColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = `${color}`;
+  changeBgColorTxt.textContent = `${color}`;
+}
